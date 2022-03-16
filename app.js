1d815b8a118c9ce181765e6ccb930b1fae3b48f2
rayaalmudena <raya.almudena@gmail.com>
2022-03-16 10:26:29 +0100

    nose

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 function removeVowels(frase) {
     // Implementar código aquí
-   
+    let pattern = /[aeiou]*/i;
+    let noVocals = frase.replaceAll(pattern, "");
+
     // Obligatorio: utilizar una expresión regular y el método de string "replace": https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/String/replace
 
     let resultado = "IMPLEMENTAME!!";
 
-    return resultado;
+    return noVocals;
 }
 console.log(removeVowels('codewars')); // --> cdwrs
 console.log(removeVowels('holaholaaa')); // -->hlhl
