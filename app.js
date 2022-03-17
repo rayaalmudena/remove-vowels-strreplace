2fe6b436d93c3bf973ecd305013c4cf3f2bdf7a0
rayaalmudena <raya.almudena@gmail.com>
2022-03-17 08:12:05 +0100

    d

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 function removeVowels(frase) {
     // Implementar código aquí
-    let pattern = /[aeiou]*/i;
+    let pattern = /[aeiou]/gi;
     let noVocals = frase.replaceAll(pattern, "");
+    //              textoPolicial.replaceAll(matriculaPattern, "PILLADO",);
 
     // Obligatorio: utilizar una expresión regular y el método de string "replace": https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/String/replace
 
